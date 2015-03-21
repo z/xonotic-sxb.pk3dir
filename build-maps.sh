e7f47296bbc4b6627db6fcb281f152cf9fbc26e3
Tyler '-z-' Mulligan <z@xnz.me>
2015-03-21 17:08:40 -0400

    map build script

---
+++ build-maps.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+if [[ ! -f "build-maps.conf" ]]; then echo "[ ERROR ] build-maps.conf does not exist. Please create one with a XONDIR variable that points to your Xonotic directory with netradiant."; exit 0; fi
+. build-maps.conf
+if [[ $1 == "-r" ]]; then echo "[ REMOVING ] Deleting old BSP files"; find maps -name *.bsp -exec rm -vf {} \;; fi
+for m in $(ls maps/*.map | grep -v ".autosave"); do $XONDIR/netradiant/install/q3map2 -meta -keepLights -v $m; done
