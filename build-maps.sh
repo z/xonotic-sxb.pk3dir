4967630ada24ca93b3c5c828c1b336c0deb8a76c
Tyler '-z-' Mulligan <z@xnz.me>
2015-10-14 21:24:01 -0400

    added quick hack to compile one map

--- build-maps.sh
+++ build-maps.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 if [[ ! -f "build-maps.conf" ]]; then echo "[ ERROR ] build-maps.conf does not exist. Please create one with a XONDIR variable that points to your Xonotic directory with netradiant."; exit 0; fi
 . build-maps.conf
+compile_map() {
+    $XONDIR/netradiant/install/q3map2 -game xonotic -fs_basepath $XONDIR -fs_game xonotic -fs_homepath ~/.xonotic -meta -keepLights -vis -fast -v $1
+}
 if [[ $1 == "-r" ]]; then echo "[ REMOVING ] Deleting old BSP files"; find maps -name *.bsp -exec rm -vf {} \;; fi
-for m in $(ls maps/*.map | grep -v ".autosave"); do $XONDIR/netradiant/install/q3map2 -game xonotic -fs_basepath $XONDIR -fs_game xonotic -fs_homepath ~/.xonotic -meta -keepLights -vis -fast -v $m; done
+if [[ $1 == "-m" ]]; then echo "[ COMPILING ] Single map: $2"; compile_map "maps/sxb1_"$2".map"; exit; fi
+for m in $(ls maps/*.map | grep -v ".autosave"); do compile_map $m; done
